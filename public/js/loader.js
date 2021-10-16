b23a27ab4f2f9ffb2fb32952d43d06e232e1e9e1
joao-simoes <joaopsimoes0@gmail.com>
2021-10-17 00:39:36 +0100

    Added loader & favicon

---
+++ public/js/loader.js
@@ -0,0 +1,11 @@
+const loader = document.getElementById('loader')
+
+$(document).ready(function() {
+
+    setTimeout(function() { loaded(); }, 2000);
+
+});
+
+function loaded() {
+    loader.style.opacity = "0%"
+}
